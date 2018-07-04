4b97b691e8f8191c0633686833e354c815b92e30
spaez <samuel005005@gmail.com>
2018-07-04 15:02:58 -0400

    Cambios al codigo

---
+++ plantillas.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//Plantilla de texto
+
+var nombre = "Samuel";//prompt("Ingrese su nombre");
+var apellidos ="Paez Perez"; //prompt("Ingrese sus apellidos");
+var texto =` 
+<h3>Mi nombre es : ${nombre} </h3>
+<h3>Mis apellidos son : ${apellidos} </h3>
+`; 
+
+document.write(texto);
