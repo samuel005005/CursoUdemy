bccb07d512942d5852f19f49834ab72cb8b3d15d
spaez <samuel005005@gmail.com>
2018-07-04 15:02:31 -0400

    Cambios al codigo

---
+++ ambito.js
@@ -0,0 +1,13 @@
+'use stric'
+
+function holaMundo(texto) {
+	var hola_mundo ='Estoy dentro de la funcion';
+	console.log(texto);
+	console.log(numero);
+	console.log(hola_mundo);
+}
+
+var texto = "Hola mundo soy una variable Global";
+var numero = 50;
+holaMundo(texto);
+console.log(hola_mundo);
